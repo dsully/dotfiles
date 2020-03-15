9a78a8c38afa319f45c1b3564784a20805a68430
Dan Sully <dsully@users.noreply.github.com>
2020-03-14 19:42:23 -0700

    This may actually work..

--- .config/fish/functions/git-link.fish
+++ .config/fish/functions/git-link.fish
@@ -1,10 +1,11 @@
-function git-link --description="Switch between different git repositories for $HOME"
+function git-link --description "Switch between different git repositories for $HOME"
 
-  if [ -n "$1" ] && [ -e "$HOME/.config/repos/$1" ]
-    echo "gitdir: .config/repos/$1" >"$HOME/.git"
-  else
-    echo "Repository \"$1\" not found in $HOME/.config/repos/"
-    ls $HOME/.config/repos/
+  if test (count $argv) -eq 0
+    echo "Repository \"$argv[1]\" not found in $HOME/.config/repos/"
+    command ls $HOME/.config/repos/
+
+  else if test -e "$HOME/.config/repos/$argv[1]"
+    echo "gitdir: .config/repos/$argv[1]" >"$HOME/.git"
   end
 
 end
