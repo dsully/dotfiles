c46f26a9c6f4db81153d34b8f051ada509dd1d74
Dan Sully <dsully@users.noreply.github.com>
2020-04-02 16:30:19 -0700

    Add zenith to Homebrew. Update git review to not amend.

--- .config/fish/functions/gr.fish
+++ .config/fish/functions/gr.fish
@@ -1,3 +1,4 @@
-function gr --wraps="git-review" -d "git review create"
-    command git review create -o --no-prompt $argv
+# Defined in /var/folders/5j/mr0j9xws5pl4yvmm38tt8lfh0003dm/T//fish.XPE4fl/gr.fish @ line 2
+function gr --wraps=git-review --description 'git review create'
+    command git review create -o --no-prompt --no-amend $argv
 end
--- .config/fish/functions/gru.fish
+++ .config/fish/functions/gru.fish
@@ -1,3 +1,4 @@
-function gru --wraps="git-review" -d "git review update"
-    command git review update --update-desc $argv
+# Defined in /var/folders/5j/mr0j9xws5pl4yvmm38tt8lfh0003dm/T//fish.GhtYAq/gru.fish @ line 2
+function gru --wraps=git-review --description 'git review update'
+    command git review update --no-amend --update-desc $argv
 end
