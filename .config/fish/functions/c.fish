c997ac07b24aeaac3045166fa0ffe7e76f3e66ea
Dan Sully <dan+github@sully.org>
2020-04-05 15:30:10 -0700

    Add git clone wrapper

---
+++ .config/fish/functions/c.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/g9/zpcnvdcj0h75srmx90gqwmkw0003dm/T//fish.qjzY3d/c.fish @ line 1
+function c --wraps='git clone' --description 'Wrap git clone.'
+    command git clone $argv
+end
