bf6c0741e20fffca3bcca3790e06ea36675bd92b
Dan Sully <dsully@users.noreply.github.com>
2020-04-09 06:15:34 -0700

    Update git functions and iTerm config.

--- .config/fish/functions/gr.fish
+++ .config/fish/functions/gr.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/5j/mr0j9xws5pl4yvmm38tt8lfh0003dm/T//fish.XPE4fl/gr.fish @ line 2
 function gr --wraps=git-review --description 'git review create'
-    command git review create -o --no-prompt --no-amend $argv
+    command git review create -o --no-amend --no-prompt $argv
 end
--- .config/fish/functions/gru.fish
+++ .config/fish/functions/gru.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/5j/mr0j9xws5pl4yvmm38tt8lfh0003dm/T//fish.GhtYAq/gru.fish @ line 2
 function gru --wraps=git-review --description 'git review update'
-    command git review update --no-amend --update-desc $argv
+    command git review update --update-desc --no-amend $argv
 end
