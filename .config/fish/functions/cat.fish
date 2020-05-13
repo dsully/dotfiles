4b23e1b5c839934d9b4f79d5b751bd5cc226306a
Dan Sully <dsully@users.noreply.github.com>
2020-05-13 14:53:11 -0700

    Update bat theme.

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,4 +1,5 @@
 # Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.DnKdnn/cat.fish @ line 2
 function cat --wraps=bat --description 'Use bat instead of cat'
-    command bat --style plain --theme base16 $argv
+    # command bat --style plain --theme base16 $argv
+    command bat --style plain --theme ansi-dark $argv
 end
