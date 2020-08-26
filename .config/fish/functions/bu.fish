4c5700584da8abeea5557b274ac9fa9fb9604d14
Dan Sully <dan+github@sully.org>
2020-08-26 04:50:14 -0700

    Update brew cu command.

--- .config/fish/functions/bu.fish
+++ .config/fish/functions/bu.fish
@@ -1,5 +1,5 @@
 function bu --wraps="brew cu" --description "Update Homebrew formula, casks and taps."
   command brew update
   command brew upgrade
-  command brew cu -a -q -y --no-quarantine $argv
+  command brew cu --all --quiet --yes --cleanup --no-brew-update --no-quarantine $argv
 end
