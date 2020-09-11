5edfd4b34f1cbf3b3bdff6cf465999a2985e9b63
Dan Sully <dsully@linkedin.com>
2020-09-11 09:23:02 -0700

    brew cu changed it's arguments.

--- .config/fish/functions/bu.fish
+++ .config/fish/functions/bu.fish
@@ -1,5 +1,5 @@
 function bu --wraps="brew cu" --description "Update Homebrew formula, casks and taps."
   command brew update
   command brew upgrade
-  command brew cu --all --quiet --yes --cleanup --no-brew-update --no-quarantine $argv
+  command brew cu --all --quiet --yes --cleanup --no-brew-update $argv
 end
