2e39bb3df167b3fa5c405708e1055aa429c338b5
Dan Sully <dan+github@sully.org>
2021-03-02 09:05:54 -0800

    Updates for fish 3.2.0

--- .config/fish/functions/addpath.fish
+++
@@ -1,8 +0,0 @@
-# Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.zQIxQn/addpath.fish @ line 2
-function addpath
-    for p in $argv
-        if begin; not contains $p $fish_user_paths; and test -d $p; end
-            set -U fish_user_paths $fish_user_paths $p
-        end
-    end
-end
