85bd4154c2d40451962a962ef88c045187418ee1
Dan Sully <dsully@users.noreply.github.com>
2021-07-04 20:18:29 -0700

    MANPATH is autoset. Use less -R for ansi-rendering. Use nord theme for bat.

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,5 +1,6 @@
 # Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.DnKdnn/cat.fish @ line 2
 function cat --wraps=bat --description 'Use bat instead of cat'
     # command bat --style plain --theme base16 $argv
-    command bat --style plain --theme ansi $argv
+    # command bat --style plain --theme ansi $argv
+    command bat --style plain --theme Nord $argv
 end
