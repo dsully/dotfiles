37c9527dee22e4796b0c3abebf89caeb02192531
Dan Sully <dsully@users.noreply.github.com>
2021-08-23 19:04:02 -0700

    Update ls alias to not conflict with another command.

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,8 +1,12 @@
-# Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.OfVEf5/ls.fish @ line 2
 function ls --wraps=/usr/local/bin/lsd --wraps=lsd
     
-  # brew tap homebrew/cask-fonts
-  # brew cask install font-hack-nerd-font
-  # iTerm2 > Preferences > Profiles > Text > Non-ASCII-Font > Change Font
-  command lsd $argv
+    # brew tap homebrew/cask-fonts
+    # brew cask install font-hack-nerd-font
+    # iTerm2 > Preferences > Profiles > Text > Non-ASCII-Font > Change Font
+    switch $OS
+        case Darwin
+          command /usr/local/bin/lsd $argv
+        case Linux
+          command /usr/bin/lsd $argv
+    end
 end
