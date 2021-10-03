f5ab3345b713d15fbe222f0337e07c4b8275f927
Dan Sully <dan+github@sully.org>
2021-10-03 15:52:45 -0700

    Remove curl-config, it conflicts with too many scripts.

--- .config/fish/functions/curl.fish
+++
@@ -1,4 +0,0 @@
-# Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.FYDQ8K/curl.fish @ line 2
-function curl --description 'Run curl with timing information.' --wraps=curl
-    command curl -s -w "@$HOME/.curl-format.txt" $argv
-end
