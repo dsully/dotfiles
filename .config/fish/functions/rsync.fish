ffadee23d87e1305443965427cdceae21a13ec91
Dan Sully <dan+github@sully.org>
2021-10-23 16:05:54 -0700

    Update rsync wrapper.

--- .config/fish/functions/rsync.fish
+++ .config/fish/functions/rsync.fish
@@ -2,5 +2,6 @@ function rsync --wraps=rsync
     # -P == --partial --progress
     # -X == extended attrs, -E == executable -A == acls  --fake-super == store priv attrs
     # -a --no-i-r --info=progress2
-    command rsync -az --no-i-r --info=progress2 --exclude '*.pyc' --exclude __pycache__ $argv
+    # --compress-choice=zstd --compress-level=3 --checksum-choice=xxh3
+    command /usr/local/bin/rsync -a --no-i-r --info=progress2 --exclude '*.pyc' --exclude __pycache__ --compress-choice=zstd --compress-level=3 --checksum-choice=xxh3 $argv
 end
