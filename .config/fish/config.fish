bf22eef7575e974e931652fc2dafa51c2f21213c
Dan Sully <dan+github@sully.org>
2021-10-29 18:16:16 -0700

    Check for Homebrew in all the right places.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,15 @@ alias view "nvim -R"
 bind --all \t complete
 # https://github.com/fish-shell/fish-shell/issues/3299
 
+# Brew is installed in different places depending on OS and architecture.
+if test -d /opt/homebrew
+    set -Ux HOMEBREW_PREFIX /opt/homebrew
+else if test -d /usr/local/Homebrew
+    set -Ux HOMEBREW_PREFIX /usr/local/Homebrew
+else if test -d /usr/local/Homebrew
+    set -Ux HOMEBREW_PREFIX /home/linuxbrew/.linuxbrew
+end
+
 fish_add_path --append \
     "$HOME/bin/share" \
     "$HOME/bin/$OS" \
@@ -37,10 +46,11 @@ fish_add_path --append \
     "$HOME/.local/bin" \
     "$HOME/.local/go/bin" \
     "$HOME/.poetry/bin" \
-    "$HOME/.local/volta/bin" \
-    /home/linuxbrew/.linuxbrew/bin \
-    /Users/dsully/Library/Python/3.9/bin \
-    /Users/dsully/Library/Python/3.10/bin \
+    "$HOME/.volta/bin" \
+    "$HOMEBREW_PREFIX/bin" \
+    "$HOMEBREW_PREFIX/sbin" \
+    "$HOMEBREW_PREFIX/opt/python@3.10/bin" \
+    "$HOMEBREW_PREFIX/opt/python@3.10/libexec/bin" \
     /Library/Apple/usr/bin \
     /usr/local/bin \
     /usr/local/sbin
@@ -49,8 +59,8 @@ if test -f "$HOME/.config/fish/lib/github.fish"
     source "$HOME/.config/fish/lib/github.fish"
 end
 
-if test -f "$HOME/.config/fish/lib/linkedin.fish"
-    source "$HOME/.config/fish/lib/linkedin.fish"
+if test -f "$HOME/.config/fish/lib/work.fish"
+    source "$HOME/.config/fish/lib/work.fish"
 end
 
 fish_add_path --append \
@@ -58,33 +68,6 @@ fish_add_path --append \
     /bin \
     /sbin \
     /usr/sbin
-
-# Set `LS_COLORS` via https://github.com/sharkdp/vivid
-if type -q vivid
-    set -gx LS_COLORS (vivid generate nord)
-end
-
-# Set Nord as the fzf color scheme: https://github.com/junegunn/fzf/blob/master/ADVANCED.md
-set -x FZF_DEFAULT_OPTS --cycle \
-    --filepath-word \
-    --height=50% \
-    --info=hidden \
-    --border=rounded \
-    --margin=1 \
-    --padding=1 \
-    --color='bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
-
-# https://github.com/PatrickF1/fzf.fish
-if type -q fzf_configure_bindings
-    set fzf_preview_file_cmd bat --line-range :100 --color=always --plain
-    set fzf_preview_dir_cmd lsd --tree --depth=1
-
-    set fzf_fd_opts --hidden --exclude ".git" --exclude ".direnv" --exclude ".pytest_cache"
-    set fzf_dir_opts --bind 'ctrl-e:execute(command nvim {} >/dev/tty)'
-
-    # Bind Ctrl-t to use fzf for the current directory.
-    fzf_configure_bindings --directory=\ct
-end
 
 # Move Golang's path out of $HOME
 set -gx GOPATH $HOME/.local/go
@@ -96,41 +79,63 @@ set -gx VOLTA_HOME "$HOME/.volta"
 # Python
 set -gx PYTHONSTARTUP ~/.config/python/startup.py
 
-# Silence direnv logging and invoke it's hook.
-if type -q direnv
-    set -gx DIRENV_LOG_FORMAT ""
-    direnv hook fish | source
-end
-
-if type -q zoxide
-    zoxide init fish --cmd j | source
-
-    set -gx _ZO_FZF_OPTS $FZF_DEFAULT_OPTS '--preview "_fzf_preview_file {}"'
-
-    alias za "zoxide add"
-    alias zq "zoxide query"
-    alias zr "zoxide remove"
-end
-
-# https://github.com/eth-p/bat-extras
-if type -q batman
-    alias man batman
-end
-
-if type -q batpipe
-    eval (batpipe)
-end
-
-# Forward term info in kitty when connection via ssh
-# if string match -q xterm-kitty -- $TERM
-#     alias ssh "kitty +kitten ssh"
-# end
-
 if test -f "/Applications/kitty.app/Contents/Resources/kitty/terminfo"
     set -gx TERMINFO "/Applications/kitty.app/Contents/Resources/kitty/terminfo"
 end
 
 if status is-interactive
+
+    # Set Nord as the fzf color scheme: https://github.com/junegunn/fzf/blob/master/ADVANCED.md
+    set -x FZF_DEFAULT_OPTS --cycle \
+        --filepath-word \
+        --height=50% \
+        --info=hidden \
+        --border=rounded \
+        --margin=1 \
+        --padding=1 \
+        --color='bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
+
+    # https://github.com/PatrickF1/fzf.fish
+    if type -q fzf_configure_bindings
+        set fzf_preview_file_cmd bat --line-range :100 --color=always --plain
+        set fzf_preview_dir_cmd lsd --tree --depth=1
+
+        set fzf_fd_opts --hidden --exclude ".git" --exclude ".direnv" --exclude ".pytest_cache"
+        set fzf_dir_opts --bind 'ctrl-e:execute(command nvim {} >/dev/tty)'
+
+        # Bind Ctrl-t to use fzf for the current directory.
+        fzf_configure_bindings --directory=\ct
+    end
+
+    # Silence direnv logging and invoke it's hook.
+    if type -q direnv
+        set -gx DIRENV_LOG_FORMAT ""
+        direnv hook fish | source
+    end
+
+    if type -q zoxide
+        zoxide init fish --cmd j | source
+
+        set -gx _ZO_FZF_OPTS $FZF_DEFAULT_OPTS '--preview "_fzf_preview_file {}"'
+
+        alias za "zoxide add"
+        alias zq "zoxide query"
+        alias zr "zoxide remove"
+    end
+
+    # Set `LS_COLORS` via https://github.com/sharkdp/vivid
+    if type -q vivid
+        set -gx LS_COLORS (vivid generate nord)
+    end
+
+    # https://github.com/eth-p/bat-extras
+    if type -q batman
+        alias man batman
+    end
+
+    if type -q batpipe
+        eval (batpipe)
+    end
 
     # make less behave with XDG
     if [ ! -d "$XDG_CACHE_HOME/less" ]
@@ -155,14 +160,9 @@ if status is-interactive
     set -gx HOMEBREW_NO_COMPAT 1
 
     # https://github.com/Homebrew/homebrew-command-not-found
-    switch $OS
-        case Darwin
-            if test -f "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
-                source /usr/local/Homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.fish
-            end
-        case Linux
-            if test -f "/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
-                source /home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.fish
-            end
+    set HANDLER Library/Taps/homebrew/homebrew-command-not-found/handler.fish
+
+    if test -f "$HOMEBREW_PREFIX/$TAPS"
+        source "$HOMEBREW_PREFIX/$TAPS"
     end
 end
