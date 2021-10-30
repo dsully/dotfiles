c8e4bc686e108ea480c292e0e136f28f05d1cf63
Dan Sully <dsully@users.noreply.github.com>
2021-10-29 19:11:35 -0700

    Don't force rsync location.

--- .config/fish/functions/rsync.fish
+++ .config/fish/functions/rsync.fish
@@ -3,5 +3,5 @@ function rsync --wraps=rsync
     # -X == extended attrs, -E == executable -A == acls  --fake-super == store priv attrs
     # -a --no-i-r --info=progress2
     # --compress-choice=zstd --compress-level=3 --checksum-choice=xxh3
-    command /usr/local/bin/rsync -a --no-i-r --info=progress2 --exclude '*.pyc' --exclude __pycache__ --compress-choice=zstd --compress-level=3 --checksum-choice=xxh3 $argv
+    command rsync -a --no-i-r --info=progress2 --exclude '*.pyc' --exclude __pycache__ --compress-choice=zstd --compress-level=3 --checksum-choice=xxh3 $argv
 end
