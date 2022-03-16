faf8fa8b37f270f32bf3a6e6ff764acfbeb1650a
Dan Sully <dan+github@sully.org>
2022-03-16 10:19:12 -0700

    Remove iTerm2

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -2,7 +2,6 @@ function ls --wraps=/usr/local/bin/lsd --wraps=lsd
 
     # brew tap homebrew/cask-fonts
     # brew cask install font-hack-nerd-font
-    # iTerm2 > Preferences > Profiles > Text > Non-ASCII-Font > Change Font
     switch $OS
         case Darwin
             command /usr/local/bin/lsd $argv
