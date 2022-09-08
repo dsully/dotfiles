aa191e7444698a027d1231bdc7cbb04a0ee5ca5e
Dan Sully <dsully@users.noreply.github.com>
2022-09-08 09:00:45 -0700

    Fix up ls wrapper.

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,11 +1,13 @@
-function ls --wraps=/usr/local/bin/lsd --wraps=lsd
+function ls --wraps=lsd
+
+    set COMMAND /bin/ls
 
     # brew tap homebrew/cask-fonts
     # brew cask install font-hack-nerd-font
-    switch $OS
-        case Darwin
-            command /usr/local/bin/lsd $argv
-        case Linux
-            command /usr/bin/lsd $argv
+    # brew install lsd
+    if test -q "$HOMEBREW_PREFIX/lsd"
+        set COMMAND "$HOMEBREW_PREFIX/lsd"
     end
+
+    command $COMMAND $argv
 end
