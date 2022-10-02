a0b9d058086e92409be35b9c9bd0735015df8671
Dan Sully <dsully@users.noreply.github.com>
2022-10-02 09:45:48 -0700

    Update Python search paths.

--- .config/fish/conf.d/python.fish
+++ .config/fish/conf.d/python.fish
@@ -3,9 +3,7 @@ if status is-interactive
     set -gx PYTHONSTARTUP $HOME/.config/python/startup.py
     set -gx PYTHON_VERSION 3.10
 
-    if [ -d "/export/apps/python/$PYTHON_VERSION/bin" ]
-        fish_add_path --append "/export/apps/python/$PYTHON_VERSION/bin"
-    end
+    fish_add_path --append "/export/apps/python/$PYTHON_VERSION/bin" "$HOME/Library/Python/$PYTHON_VERSION/bin"
 
     # Python PEP-582 PDM integration: https://pdm.fming.dev
     set -gx PYTHONPATH "$HOME/.local/pipx/venvs/pdm/lib/python$PYTHON_VERSION/site-packages/pdm/pep582"
