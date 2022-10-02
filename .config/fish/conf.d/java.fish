1e477edcdc87e256bef9bc51d8e9bedf79d2bfb3
Dan Sully <dsully@users.noreply.github.com>
2022-10-02 15:51:19 -0700

    Fix setting JAVA_HOME on macOS.

--- .config/fish/conf.d/java.fish
+++ .config/fish/conf.d/java.fish
@@ -3,9 +3,9 @@ if status is-interactive
     switch (uname)
         case Darwin
             set -l BASE /Library/Java/JavaVirtualMachines
-            set -gx JAVA_HOME $BASE/(command ls $BASE|sort)[-1]
+            set -gx JAVA_HOME $BASE/(command ls $BASE|sort)[-1]/Contents/Home
 
-            fish_add_path --append $JAVA_HOME/Contents/Home/bin
+            fish_add_path --append $JAVA_HOME/bin
         case Linux
             set -gx JAVA_HOME (dirname (dirname (readlink -f /usr/bin/java)))
     end
