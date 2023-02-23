152e39c12604a195741d7ca5e6b6f4c970dd3906
Dan Sully <dan+github@sully.org>
2023-02-23 09:03:13 -0800

    fish: Update ls wrapper.

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,13 +1,11 @@
 function ls --wraps=lsd
 
-    set COMMAND /bin/ls
-
     # brew tap homebrew/cask-fonts
     # brew cask install font-hack-nerd-font
     # brew install lsd
-    if test -q "$HOMEBREW_PREFIX/lsd"
-        set COMMAND "$HOMEBREW_PREFIX/lsd"
+    if type -q lsd
+        command lsd $argv
+    else
+        command ls $argv
     end
-
-    command $COMMAND $argv
 end
