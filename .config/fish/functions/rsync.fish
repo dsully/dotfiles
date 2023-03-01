b9bbea2edf5285a5d26bec96e732d9cc4475e332
Dan Sully <dan+github@sully.org>
2023-03-01 15:10:36 -0800

    fish: Remove rsync wrapper as it's causing issues.

--- .config/fish/functions/rsync.fish
+++
@@ -1,7 +0,0 @@
-function rsync --wraps=rsync
-    # -P == --partial --progress
-    # -X == extended attrs, -E == executable -A == acls  --fake-super == store priv attrs
-    # -a --no-i-r --info=progress2
-    # --compress-choice=zstd --compress-level=3 --checksum-choice=xxh3
-    command rsync -a --no-i-r --info=progress2 --exclude '*.pyc' --exclude __pycache__ --compress-choice=zstd --compress-level=3 --checksum-choice=xxh3 $argv
-end
