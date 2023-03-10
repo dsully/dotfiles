ea70aee6894d03a7b928a8d1589bc7e5e43f5d49
Dan Sully <dsully@users.noreply.github.com>
2023-03-09 16:36:45 -0800

    fish: More zellij attach changes.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -179,18 +179,12 @@ if status is-interactive
     # https://zellij.dev/ - terminal multiplexer and session manager.
     if type -q zellij
         if [ ! -f "$XDG_CONFIG_HOME/fish/completions/zellij.fish" ]
-            zellij setup --generate-completion fish >$XDG_CONFIG_HOME/fish/completions/zellij.fish
+            zellij setup --generate-completion fish > "$XDG_CONFIG_HOME/fish/completions/zellij.fish"
         end
 
         # Automatically attach if a session doesn't exist.
-        if not set -q ZELLIJ
-            and test $HOSTNAME = server
-
-            if test -n (zellij list-sessions | grep -c server)
-                zellij attach server
-            else
-                zellij -s server
-            end
+        if not set -q ZELLIJ; and test -e "$XDG_CONFIG_HOME/zellij/auto"
+            zellij attach -c $HOSTNAME
         end
     end
 end
