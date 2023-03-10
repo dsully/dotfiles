2c792672b026f8cd1b306ab1c99aa967e1abda1c
Dan Sully <dsully@users.noreply.github.com>
2023-03-10 06:05:58 -0800

    chore: Update fish & bootstrap files.

--- .config/fish/functions/unzip.fish
+++ .config/fish/functions/unzip.fish
@@ -1,7 +1,7 @@
 # Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.y5SBcJ/unzip.fish @ line 2
 function unzip
-    if test -d /usr/local/opt/unzip/bin
-        command /usr/local/opt/unzip/bin/unzip $argv
+    if test -d $HOMEBREW_PREFIX/opt/unzip/bin
+        command $HOMEBREW_PREFIX/opt/unzip/bin/unzip $argv
     else
         command unzip $argv
     end
