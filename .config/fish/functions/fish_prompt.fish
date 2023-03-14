52b9a133e61ebbae4f28521c987b43d7794eef6b
Dan Sully <dsully@users.noreply.github.com>
2023-03-14 06:28:14 -0700

    zellij: Add borderless layout.

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -5,10 +5,9 @@ set -U __fish_git_prompt_color_branch -o white
 set -U fish_color_user cyan
 set -U fish_color_host cyan
 
-# only display a host name if we're in an ssh session, but not inside zellij, as it provides the hostname.
+# only display a host name if we're in an ssh session.
 function __user_host
     if test -n "$SSH_CLIENT$SSH_TTY"
-        and not set -q ZELLIJ
 
         if test (id -u) -eq 0
             set_color --bold red
