fc399401c47ab4c3e10dc8ecc040bfe21b4e28a3
Dan Sully <dan+github@sully.org>
2023-03-14 17:46:37 -0700

    Update cat function to handle more markdown types and kitty/icat graphics.

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,8 +1,23 @@
 # Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.DnKdnn/cat.fish @ line 2
 function cat --wraps=bat --description 'Use bat instead of cat'
 
-    if string match --quiet --regex ".*\.md\$" $argv[1]
+    # https://sw.kovidgoyal.net/kitty/kittens/icat/
+    set -f icat_extensions png jpg gif bmp tiff webp
+
+    set -f glow_extensions md markdown mkd text txt
+
+    if isatty stdin; and not set -q argv[1]
+        command cat -
+        return
+    end
+
+    set -f ext (string split "." $argv[1])[-1]
+
+    if contains $ext $glow_extensions
         glow -p $argv[1]
+
+    else if contains $ext $icat_extensions; and type -q kitty
+        kitty +kitten icat $argv[1]
     else
         command bat --style plain --theme Nord $argv
     end
