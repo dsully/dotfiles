54f2f76c632ee87fa90635697600e44854908a52
Dan Sully <dan+github@sully.org>
2023-03-24 05:59:27 -0700

    rtx: Fix path to prevent errors on upgrades.

--- .config/fish/conf.d/rtx.fish
+++ .config/fish/conf.d/rtx.fish
@@ -1,8 +1,8 @@
 # https://github.com/jdxcode/rtx
-if status is-interactive; and type -q rtx
+if status is-interactive; and test -f $HOMEBREW_PREFIX/bin/rtx
     set -gx RTX_EXPERIMENTAL 1
     set -gx RTX_USE_TOML 1
     set -gx RTX_PYTHON_DEFAULT_PACKAGES_FILE $XDG_CONFIG_HOME/rtx/default-python-packages.txt
 
-    rtx activate fish | source
+    $HOMEBREW_PREFIX/bin/rtx activate fish | source
 end
