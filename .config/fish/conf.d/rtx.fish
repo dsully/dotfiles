507a197a93ba3daec473424a22578413bff95306
Dan Sully <dsully@users.noreply.github.com>
2023-03-25 20:31:33 -0700

    rtx: update config.

--- .config/fish/conf.d/rtx.fish
+++ .config/fish/conf.d/rtx.fish
@@ -1,8 +1,6 @@
 # https://github.com/jdxcode/rtx
 if status is-interactive; and test -f $HOMEBREW_PREFIX/bin/rtx
-    set -gx RTX_EXPERIMENTAL 1
     set -gx RTX_USE_TOML 1
-    set -gx RTX_PYTHON_DEFAULT_PACKAGES_FILE $XDG_CONFIG_HOME/rtx/default-python-packages.txt
 
     $HOMEBREW_PREFIX/bin/rtx activate fish | source
 end
