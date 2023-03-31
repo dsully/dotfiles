5d81908f8203f9c90b385c0134174a3b168e4248
Dan Sully <dsully@users.noreply.github.com>
2023-03-31 16:33:11 -0700

    fish: Clean up 'man' wrapper.

--- .config/fish/functions/man.fish
+++ .config/fish/functions/man.fish
@@ -1,19 +1,16 @@
 # colors documentation can be found at
 # http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html#256-colors
 function man -d "Run man with added colors" --wraps=man
-    set -l bold_ansi_code "\u001b[1m"
-    set -l underline_ansi_code "\u001b[4m"
-    set -l reversed_ansi_code "\u001b[7m"
-    set -l reset_ansi_code "\u001b[0m"
-    set -l teal_ansi_code "\u001b[38;5;31m"
-    set -l green_ansi_code "\u001b[38;5;70m"
-    set -l gold_ansi_code "\u001b[38;5;220m"
 
-    set -x LESS_TERMCAP_md (printf $bold_ansi_code$teal_ansi_code) # start bold
-    set -x LESS_TERMCAP_me (printf $reset_ansi_code) # end bold
-    set -x LESS_TERMCAP_us (printf $underline_ansi_code$green_ansi_code) # start underline
-    set -x LESS_TERMCAP_ue (printf $reset_ansi_code) # end underline
-    set -x LESS_TERMCAP_so (printf $reversed_ansi_code$gold_ansi_code) # start standout
-    set -x LESS_TERMCAP_se (printf $reset_ansi_code) # end standout
+    set -l end (printf "\e[0m")
+
+    set -lx LESS_TERMCAP_mb (set_color bf616a) #bf616a
+    set -lx LESS_TERMCAP_md (set_color -o 81a1c1) #81a1c1
+    set -lx LESS_TERMCAP_me $end
+    set -lx LESS_TERMCAP_so (set_color d8dee9) #d8dee9
+    set -lx LESS_TERMCAP_se $end
+    set -lx LESS_TERMCAP_us (set_color -u 8fbcbb) #8fbcbb
+    set -lx LESS_TERMCAP_ue $end
+
     command man $argv
 end
