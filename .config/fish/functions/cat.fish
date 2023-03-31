d665929503a39f04d23ae58d1030333487ed830f
Dan Sully <dsully@users.noreply.github.com>
2023-03-31 13:35:15 -0700

    fish: Update cat to handle wezterm

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,4 +1,3 @@
-# Defined in /var/folders/3q/x_nn53z13p35x0qrrx7n8bz40003dm/T//fish.DnKdnn/cat.fish @ line 2
 function cat --wraps=bat --description 'Use bat instead of cat'
 
     if isatty stdin; and not set -q argv[1]
@@ -7,19 +6,22 @@ function cat --wraps=bat --description 'Use bat instead of cat'
     end
 
     # https://sw.kovidgoyal.net/kitty/kittens/icat/
-    set -f icat_extensions png jpg gif bmp tiff webp
+    # https://github.com/wez/wezterm/blob/main/docs/imgcat.md
+    set -f icat_extensions png jpg jpeg gif bmp tiff webp
     set -f glow_extensions md markdown mkd text txt
 
     # Escape --version and similar.
-    set -f arg (string replace -r '^-' '\-')
-
-    set -f ext (string split "." $arg)[-1]
+    set -f ext (string split "." -- $argv[1])[-1]
 
     if contains $ext $glow_extensions
         glow -p $argv[1]
 
-    else if contains $ext $icat_extensions; and type -q kitty
+    else if contains $ext $icat_extensions; and test $TERM = xterm-kitty
         kitty +kitten icat $argv[1]
+
+    else if contains $ext $icat_extensions; and type -q wezterm
+        wezterm imgcat $argv[1]
+
     else
         command bat --style plain --theme Nord $argv
     end
