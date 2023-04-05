5dff684fafdb12c8356afe9082d3f203e3367662
Dan Sully <dsully@users.noreply.github.com>
2023-04-04 17:07:05 -0700

    wezterm: Update TERMINFO for different paths.

--- .config/fish/conf.d/wezterm.fish
+++ .config/fish/conf.d/wezterm.fish
@@ -4,9 +4,13 @@ if status is-interactive
 
     if test $TERM = wezterm
         set -l TERMINFO_DIR $HOME/.terminfo
-        set -l WEZ_TERMINFO $TERMINFO_DIR/77/wezterm
 
-        if not test -f $WEZ_TERMINFO
+        # Different OSes, different paths.
+        set -l WEZ_TERMINFO1 $TERMINFO_DIR/77/wezterm
+        set -l WEZ_TERMINFO2 $TERMINFO_DIR/w/wezterm
+
+        if not test -f $WEZ_TERMINFO1
+            -o not test -f $WEZ_TERMINFO2
             /usr/bin/tic -x -o $TERMINFO_DIR $XDG_CONFIG_HOME/wezterm/wezterm.terminfo
         end
     end
