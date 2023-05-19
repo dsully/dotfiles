c2b7a9d76d9be0a41e9ff4c050b8a023663d349d
Dan Sully <dsully@users.noreply.github.com>
2023-05-18 17:56:31 -0700

    Add rye config.

---
+++ .config/fish/conf.d/rye.fish
@@ -0,0 +1,11 @@
+# https://rye-up.com
+if status is-interactive
+
+    if test -d $HOME/.rye
+        set -gx RYE_HOME $HOME/.rye
+    end
+
+    if test -d $RYE_HOME/shims
+        fish_add_path --append $RYE_HOME/shims
+    end
+end
