4a610d0ef3334703b5882df386b263b93b8cf5db
Dan Sully <dsully@users.noreply.github.com>
2023-05-26 16:38:15 -0700

    fish: open changes.
    Handle local opening better.
    
    Pass through arguments with a leading dash properly.
    
    Get the URL from git-open as otherwise I can't background it.

--- .config/fish/functions/open.fish
+++ .config/fish/functions/open.fish
@@ -1,9 +1,6 @@
 function open --description 'Remotely open a for the current or given path.'
 
-    if not set -q SSH_TTY
-        command open $argv
-        return 0
-    end
+    set -f url false
 
     # Set the path if we have arguments.
     if test (count $argv) -gt 0
@@ -11,47 +8,58 @@ function open --description 'Remotely open a for the current or given path.'
     else
         # If this is a git repository, and we have git-open, open the remote URL.
         if type -q git-open
-            if test (command git-open) -eq 0
-                return 0
-            end
+            set -f path (command git-open --print)
+        else
+            # Otherwise use the PWD if no arguments were passed.
+            set -f path $PWD
         end
-
-        # Otherwise use the PWD if no arguments were passed.
-        set -f path $PWD
     end
 
     # Pass through URLs. TODO: Handle file:/// URLs?
-    if string match -q "://" $path
+    if string match -e -q "://" -- $path
         set -f remote $path
+        set -f url true
 
-    # If run with a - option, pass it through to macOS open.
-    else if string match -q -r "^-" $path
+    else if string match -e -q "-" -- $path
+        # If run with a - option, pass it through to macOS open.
         command open $path
         return 0
 
-    else
+    else if set -q SSH_TTY
         # Clean up the path.
-        set -f path (string escape -n (realpath $path))
+        set -f path (string escape -n -- (realpath -- $path))
 
-        if string match -q -r "^/bits" $path
+        if string match -q -r "^/bits" -- $path
             set -l partial (string replace /bits/ "" $path)
             set -f remote "~/Mounts/$hostname/$partial"
 
-        else if string match -q -r "^$HOME" $path
+        else if string match -q -r "^$HOME" -- $path
 
             switch $OS
                 case Darwin
-                    set -f remote (string replace $HOME /home/$USER $path)
+                    set -f remote (string replace $HOME /home/$USER -- $path)
                 case Linux
-                    set -f remote (string replace $HOME /Users/$USER $path)
+                    set -f remote (string replace $HOME /Users/$USER -- $path)
             end
         end
     end
 
-    # https://github.com/superbrothers/opener
-    if test -e $HOME/.opener.sock
-        echo $remote | nc -U "$HOME/.opener.sock"
+    if not set -q remote
+        set -f remote $path
+    end
+
+    if set -q SSH_TTY
+        # https://github.com/superbrothers/opener
+        if test -e $HOME/.opener.sock
+            echo $remote | nc -U "$HOME/.opener.sock"
+        else
+            echo "Install opener: https://github.com/superbrothers/opener"
+        end
     else
-        echo "Install opener: https://github.com/superbrothers/opener"
+        if set -q url
+            command open --background $remote
+        else
+            command open $remote
+        end
     end
 end
