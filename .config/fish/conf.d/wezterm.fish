2019b87c778fc9ffe34ecfd4ad1ec6dbcf6a6f1d
Dan Sully <dan+github@sully.org>
2023-06-02 07:06:49 -0700

    fish: Update wezterm terminfo logic

--- .config/fish/conf.d/wezterm.fish
+++ .config/fish/conf.d/wezterm.fish
@@ -2,19 +2,13 @@
 # https://wezfurlong.org/wezterm/config/lua/config/term.html?h=environment
 if status is-interactive
 
-    if test $TERM = wezterm
+    if test $TERM = wezterm; and not infocmp wezterm &>/dev/null
         set -l TERMINFO_DIR $HOME/.terminfo
 
-        # Different OSes, different paths.
-        switch $OS
-            case Darwin
-                set -f WEZ_TERMINFO $TERMINFO_DIR/77/wezterm
-            case Linux
-                set -f WEZ_TERMINFO $TERMINFO_DIR/w/wezterm
+        if fish_is_root_user
+            set TERMINFO_DIR /usr/share/terminfo
         end
 
-        if not test -f $WEZ_TERMINFO
-            /usr/bin/tic -x -o $TERMINFO_DIR $XDG_CONFIG_HOME/wezterm/terminfo
-        end
+        /usr/bin/tic -x -o $TERMINFO_DIR $XDG_CONFIG_HOME/wezterm/terminfo
     end
 end
