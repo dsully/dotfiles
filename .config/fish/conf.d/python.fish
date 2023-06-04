013ef80efa2dc9486221c4751183393f46a373bd
Dan Sully <dsully@users.noreply.github.com>
2023-06-03 19:03:15 -0700

    fish: Add some environment variables.

--- .config/fish/conf.d/python.fish
+++ .config/fish/conf.d/python.fish
@@ -1,5 +1,10 @@
 if status is-interactive
 
+    set -gx PIP_DISABLE_PIP_VERSION_CHECK 1
+    set -gx PIP_REQUIRE_VIRTUALENV 1
+    set -gx PIPX_DEFAULT_PYTHON $HOMEBREW_PREFIX/bin/python3 # don't break venv's on brew upgrade
+    set -gx PYTHONDONTWRITEBYTECODE 1
+
     set -l startup $XDG_CONFIG_HOME/python/startup.py
 
     if test -f $startup
