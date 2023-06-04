bfde4ba8aa7b413e4e693d182a4ad1445706a046
Dan Sully <dan+github@sully.org>
2023-06-03 19:03:15 -0700

    fish: Add some environment variables.

--- .config/fish/conf.d/02-homebrew.fish
+++ .config/fish/conf.d/02-homebrew.fish
@@ -1,6 +1,6 @@
 if status is-interactive
 
-# Brew is installed in different places depending on OS and architecture.
+    # Brew is installed in different places depending on OS and architecture.
     if test -d /opt/homebrew
         set -Ux HOMEBREW_PREFIX /opt/homebrew
     else if test -d /usr/local/Homebrew
@@ -17,6 +17,13 @@ if status is-interactive
         set -gx HOMEBREW_NO_ANALYTICS 1
         set -gx HOMEBREW_NO_COMPAT 1
         set -gx HOMEBREW_NO_ENV_HINTS 1
+
+        # Tell build tools about our special prefix
+        if test $HOMEBREW_PREFIX != /usr/local
+            set -gx CFLAGS -I$HOMEBREW_PREFIX/include
+            set -gx CPPFLAGS -I$HOMEBREW_PREFIX/include
+            set -gx LDFLAGS -L$HOMEBREW_PREFIX/lib
+        end
 
         # https://github.com/Homebrew/homebrew-command-not-found
         set HANDLER Library/Taps/homebrew/homebrew-command-not-found/handler.fish
