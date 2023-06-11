4f016838e3bd0ab95185a6347b15aacb9438a228
Dan Sully <dsully@users.noreply.github.com>
2023-06-11 16:14:24 -0700

    fish/nvim: Use pbcopy/pbpaste over SSH.
    Update nvim config for the clipboard.
    
    Use xdg-open-svc from the Homebrew tap.
    
    Update fish open function & xdg-open wrapper.

--- .config/fish/functions/tower.fish
+++ .config/fish/functions/tower.fish
@@ -1,5 +1,3 @@
-function tower -d "Open Tower for directory (default CWD)"
-    open -a Tower (fallback $argv .)
-
-    # alias tower="gittower (git rev-parse --show-toplevel)"
+function tower -d "Open Tower for directory (default: Git root)"
+    command open -a Tower (fallback $argv (git rev-parse --show-toplevel))
 end
