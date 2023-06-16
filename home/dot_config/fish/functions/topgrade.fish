5b3dd5d78d0fd79a0d79491509cf669e0af9b520
Dan Sully <dan+github@sully.org>
2023-06-16 08:48:50 -0700

    topgrade: Templatize config file.

--- home/dot_config/fish/functions/topgrade.fish
+++
@@ -1,8 +0,0 @@
-function topgrade -d "Topgrade wrapper."
-
-    command topgrade --config $XDG_CONFIG_HOME/topgrade/default.toml $argv
-
-    if test -f $XDG_CONFIG_HOME/topgrade/$HOSTNAME.toml
-        command topgrade --config $XDG_CONFIG_HOME/topgrade/$HOSTNAME.toml $argv
-    end
-end
