69254bd4f33f54fd65df703b2478e322b9535c2e
Dan Sully <dsully@users.noreply.github.com>
2023-06-19 11:05:16 -0700

    fish: Add jless in cat.fish; Set MOAR options properly; Set BAT_PAGER

--- home/dot_config/fish/functions/cat.fish
+++ home/dot_config/fish/functions/cat.fish
@@ -9,27 +9,33 @@ function cat --wraps=bat --description 'Use bat instead of cat'
     # https://github.com/wez/wezterm/blob/main/docs/imgcat.md
     set -f image_extensions png jpg jpeg gif bmp tiff webp
     set -f markdown_extensions md markdown mkd
+    set -f structured_extensions json yaml yml
 
     # Escape --version and similar.
     set -f ext (string split "." -- $argv[1])[-1]
 
-    if contains $ext $markdown_extensions
+    set -f md_exe type -q frogmouth; or type -q glow
 
+    if contains $ext $markdown_extensions; and $md_exe
+
+        # https://github.com/Textualize/frogmouth
         if type -q frogmouth
             frogmouth $argv[1]
         else if type -q glow
             glow -p $argv[1]
-        else
-            echo "Neither 'frogmouth' nor 'glow' is installed for Markdown rendering."
         end
 
+    # https://jless.io
+    else if contains $ext $structured_extensions; and type -q jless
+        jless $argv
+
     else if contains $ext $image_extensions; and test $TERM = xterm-kitty
-        kitty +kitten icat $argv[1]
+        kitty +kitten icat $argv
 
     else if contains $ext $image_extensions; and type -q wezterm
-        wezterm imgcat $argv[1]
+        wezterm imgcat $argv
 
     else
-        command bat --style plain --theme Nord --pager=$PAGER $argv
+        command bat --style plain --theme Nord $argv
     end
 end
