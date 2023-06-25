f08e253c2e82adfe74b0dc9dc95806d9c2cb1852
Dan Sully <dsully@users.noreply.github.com>
2023-06-25 10:42:59 -0700

    fish: Update tower wrapper.

--- home/dot_config/fish/functions/tower.fish
+++ home/dot_config/fish/functions/tower.fish
@@ -1,6 +1,6 @@
 function tower -d "Open Tower for directory (default: Git root)"
 
-    if test $PWD = $HOME; or string match -q -r $XDG_CONFIG_HOME $PWD
+    if test $PWD = $HOME
         command open -a Tower "$XDG_DATA_HOME/chezmoi"
     else
         command open -a Tower (fallback $argv (git rev-parse --show-toplevel))
