dcb4ddd24c71668ba47868f9312f87d5ebaf1606
Dan Sully <dan+github@sully.org>
2023-08-14 14:21:24 -0700

    rust: Move config to environment.

--- home/dot_config/fish/conf.d/rust.fish
+++ home/dot_config/fish/conf.d/rust.fish
@@ -1,6 +1,9 @@
 # Also use https://github.com/holmgr/cargo-sweep
 if status is-interactive
 
+    # https://doc.rust-lang.org/cargo/reference/environment-variables.html
+    set -gx CARGO_NET_GIT_FETCH_WITH_CLI true
+
     if type -q sccache
         set -gx RUSTC_WRAPPER sccache
     end
