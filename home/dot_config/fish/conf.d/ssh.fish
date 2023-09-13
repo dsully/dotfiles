6c119f63e223755483e3e15e0c202b06dbf302c3
Dan Sully <dsully@users.noreply.github.com>
2023-09-13 16:43:25 -0700

    Fix hostname.

--- home/dot_config/fish/conf.d/ssh.fish
+++ home/dot_config/fish/conf.d/ssh.fish
@@ -1,4 +1,4 @@
-if status is-interactive; and test $HOSTNAME = dsully-md1
+if status is-interactive; and test $HOSTNAME = dsully-mn1
 
     # Disable work's forced ssh-agent and use 1Password's ssh-agent instead.
     ssh-add -D -q
