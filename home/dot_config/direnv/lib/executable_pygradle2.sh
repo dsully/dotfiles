2f601dbba7bb4bd0a085ea4f9fa5851de3adc352
Dan Sully <dsully@users.noreply.github.com>
2023-11-22 13:12:31 -0800

    direnv: Functions update.

--- home/dot_config/direnv/lib/executable_pygradle2.sh
+++ home/dot_config/direnv/lib/executable_pygradle2.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-
 layout_pygradle2() {
     REPO="$(basename "$PWD")"
     VIRTUAL_ENV="$(expand_path "../build/$REPO/environments/development-venv")"
 
-    if [ -x "$VIRTUAL_ENV/bin/python" ]; then
-        PATH_add "$VIRTUAL_ENV/bin"
+    PATH_add "$VIRTUAL_ENV/bin"
 
-        export VIRTUAL_ENV
-    fi
+    export VIRTUAL_ENV
 }
