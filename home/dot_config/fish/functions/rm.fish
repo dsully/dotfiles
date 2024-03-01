b7f8c0ea3302755606376d9b3c93a4b90bdede94
Dan Sully <dan+github@sully.org>
2024-03-01 09:17:33 -0800

    Update rip/rm wrapper.

--- home/dot_config/fish/functions/rm.fish
+++ home/dot_config/fish/functions/rm.fish
@@ -1,8 +1,14 @@
 function rm --wraps=rip --description 'Safe remove'
-
     if type -q rip
+        # Check if the first argument is "-r" or "-rf" and remove it if it matches
+        if string match -q -e -- "-r*" $argv[1]
+            set -e argv[1]
+        end
+
+        # Call `rip` with the potentially modified arguments
         command rip $argv
     else
+        # Fallback to regular `rm` if `rip` is not available
         command rm $argv
     end
 end
