f76966f795f6e4cb1e48c2d99e8dbdc72ba2d3ea
Dan Sully <dan+github@sully.org>
2024-03-09 16:53:38 -0800

    Fix hostname generation on macOS

--- home/.chezmoiscripts/.01-hostnamectl.sh
+++ home/.chezmoiscripts/.01-hostnamectl.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env sh
 
-if [ "$(lsb_release -is)" = "CentOS" ]; then
-    CHASSIS=$(hostnamectl | grep Chassis | awk '{print $2}')
+OS=$(uname)
 
-    echo "{ \"Chassis\": \"$CHASSIS\" }"
+if [ "$OS" = "Darwin" ]; then
+
+    CHASSIS=$(/usr/sbin/system_profiler -json SPHardwareDataType | grep MacBook)
+
+    if [ "$CHASSIS" ]; then
+        CHASSIS=laptop
+    else
+        CHASSIS=desktop
+    fi
+
+    HOSTNAME=$(scutil --get ComputerName | cut -d'.' -f1)
+
+    echo "{ \"Chassis\": \"$CHASSIS\", \"Hostname\": \"$HOSTNAME\"}"
+
 else
-    hostnamectl --json=short
+    if [ "$(lsb_release -is)" = "CentOS" ]; then
+        CHASSIS=$(hostnamectl | grep Chassis | awk '{print $2}')
+        HOSTNAME=$(hostnamectl | grep StaticHostname | awk '{print $2}')
+
+        echo "{ \"Chassis\": \"$CHASSIS\", \"Hostname\": \"$HOSTNAME\"}"
+    else
+        hostnamectl --json=short
+    fi
 fi
