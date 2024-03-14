09b65081552113113a9167d254cf5ad84f4b2339
Dan Sully <dsully@users.noreply.github.com>
2024-03-13 19:20:49 -0700

    bootstrap: Update formatting and shellcheck

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,37 +1,37 @@
 #!/bin/sh -e
 
-if ! command -v chezmoi >/dev/null; then
-    bin_dir="${XDG_BIN_HOME:-$HOME/.local/bin}"
-    chezmoi="$bin_dir/chezmoi"
+if ! command -v chezmoi > /dev/null; then
+  bin_dir="${XDG_BIN_HOME:-$HOME/.local/bin}"
+  chezmoi="$bin_dir/chezmoi"
 
-    chezmoi_url="https://www.chezmoi.io/get"
+  chezmoi_url="https://www.chezmoi.io/get"
 
-    if command -v curl >/dev/null; then
-        curl -sSf "$chezmoi_url" | sh -s -- -b "$bin_dir"
-    else
-        echo "To install chezmoi, you must have curl installed." >&2
-        exit 1
-    fi
+  if command -v curl > /dev/null; then
+    curl -sSf "$chezmoi_url" | sh -s -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl installed." >&2
+    exit 1
+  fi
 else
-    chezmoi=chezmoi
+  chezmoi=chezmoi
 fi
 
 # Attach to a TTY for input prompting.
 stdin='/dev/null'
 
 if [ "$(ps otty= $$)" != '?' ]; then
-    stdin='/dev/tty'
+  stdin='/dev/tty'
 fi
 
 # Set the path for everything else.
-export PATH=/opt/homebrew/bin:/home/linuxbrew:$HOME/.cargo/bin:$HOME/.local/bin:$PATH
+export PATH=/opt/homebrew/bin:/home/linuxbrew:"$HOME"/.cargo/bin:"$HOME"/.local/bin:"$PATH"
 
-"$chezmoi" init --apply --exclude encrypted "$@" "$USER" <$stdin
+"$chezmoi" init --apply --exclude encrypted "$@" "$USER" < "$stdin"
 
 # Re-run to generate data.toml.
 if [ $? -eq 0 ]; then
-    "$chezmoi" apply --force
+  "$chezmoi" apply --force
 
-    echo ""
-    echo "Bootstrap complete! Restart your terminal to load your new environment!"
+  echo ""
+  echo "Bootstrap complete! Restart your terminal to load your new environment!"
 fi
