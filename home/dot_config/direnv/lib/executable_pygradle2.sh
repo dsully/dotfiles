45453ec23a3a48eeaba3353e8731c1f2bea3f7c6
Dan Sully <dsully@users.noreply.github.com>
2024-04-01 10:42:27 -0700

    direnv: Update settings / library. Remove auto.sh

--- home/dot_config/direnv/lib/executable_pygradle2.sh
+++
@@ -1,8 +0,0 @@
-layout_pygradle2() {
-    REPO="$(basename "$PWD")"
-    VIRTUAL_ENV="$(expand_path "../build/$REPO/environments/development-venv")"
-
-    PATH_add "$VIRTUAL_ENV/bin"
-
-    export VIRTUAL_ENV
-}
