f9db0906bc63e9a62b59f5f7fcb7dc071cf232b1
Dan Sully <dsully@users.noreply.github.com>
2024-06-13 09:32:33 -0700

    fish: Update 'gp' function to use 'git prune-branches'

--- home/dot_config/fish/functions/gp.fish
+++ home/dot_config/fish/functions/gp.fish
@@ -1,3 +1,12 @@
 function gp --wraps="git pull" -d "git pull rebase"
+
+    echo "Pulling with rebase and autostash ..."
+
     command git pull --rebase --autostash $argv
+
+    if test $status -eq 0
+
+        echo "Pruning local branches that have been merged ..."
+        git prune-branches
+    end
 end
