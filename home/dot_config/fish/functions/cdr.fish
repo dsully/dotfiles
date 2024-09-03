7f69faab4a1aba93e5d0c5c37cc44cf0587dd7e1
Dan Sully <dan+github@sully.org>
2024-09-03 08:03:43 -0700

    fish: Update cd to git root function

--- home/dot_config/fish/functions/cdr.fish
+++ home/dot_config/fish/functions/cdr.fish
@@ -1,3 +1,10 @@
 function cdr --description "chdir to the root of a git repo."
-    cd (git rev-parse --show-toplevel 2> /dev/null)
+    set -l repo (command git rev-parse --git-dir --is-inside-git-dir --is-bare-repository --is-inside-work-tree --short HEAD 2> /dev/null)
+    test -n "$repo"; or return
+
+    set -l cd_up_path (command git rev-parse --show-cdup)
+
+    if test -n $cd_up_path
+        cd $cd_up_path
+    end
 end
