aa0dd1442bd1c3c061e668cc05deff0b94e26816
Dan Sully <dsully@users.noreply.github.com>
2024-09-03 07:40:42 -0700

    fish: Update interfaces script

--- home/dot_config/fish/functions/interfaces.fish
+++ home/dot_config/fish/functions/interfaces.fish
@@ -46,13 +46,53 @@ function interfaces
         return 1
     end
 
+    # Create associative arrays to store interfaces by type
+    set -l ethernet_interfaces
+    set -l loopback_interfaces
+    set -l wifi_interfaces
+    set -l other_interfaces
+
+    set -lx blue '\x1B\e[34m'
+    set -lx green '\x1B\e[32m'
+    set -lx reset '\x1B\e[0m'
+
     for interface in $net_interfaces
         set -l device (string split ":" $interface)[1]
         set -l interface_type (string split ":" $interface)[2]
         set -l ip_address (get_ip_address $device)
 
         if test -n "$ip_address"
-            echo "$device: $ip_address - $interface_type"
+            set -l entry (printf "%-18s %s" $device (echo -en "$blue$ip_address$reset"))
+
+            switch $interface_type
+                case "USB 10/100/1G/2.5G LAN"
+                    set -a ethernet_interfaces $entry
+                case Ethernet
+                    set -a ethernet_interfaces $entry
+                case Loopback
+                    set -a loopback_interfaces $entry
+                case Wi-Fi
+                    set -a wifi_interfaces $entry
+                case '*'
+                    set -a other_interfaces $entry
+            end
         end
     end
+
+    # Sort and print interfaces by type
+    function print_interfaces
+        set -l type $argv[1]
+        set -l interfaces $argv[2..-1]
+
+        if test (count $interfaces) -gt 0
+            echo -e $green$type$reset":"
+            printf "  %s\n" (string join \n $interfaces | sort)
+            echo
+        end
+    end
+
+    print_interfaces Ethernet $ethernet_interfaces
+    print_interfaces Loopback $loopback_interfaces
+    print_interfaces Wi-Fi $wifi_interfaces
+    print_interfaces Other $other_interfaces
 end
