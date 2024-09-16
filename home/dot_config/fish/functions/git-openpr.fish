4ddf32f249a0fb7403f0ca4872e32e2fe735a020
Dan Sully <dsully@users.noreply.github.com>
2024-09-16 14:03:09 -0700

    fish: Fix git-openpr

--- home/dot_config/fish/functions/git-openpr.fish
+++ home/dot_config/fish/functions/git-openpr.fish
@@ -1,11 +1,17 @@
 # Open the Pull Request URL for your current directory's branch, merges by default against the current branch's parent
 function git-openpr --description 'Open the Pull Request URL for your current directory'
-    set github_url (git remote -v | grep 'origin.*fetch' | awk '{print $2}' | sed -Ee 's#(git@|git://)#https://#' -e 's@com:@com/@' -e 's%\.git$%%' | awk '/github/')
-    set current_branch_name (git rev-parse --abbrev-ref HEAD)
 
     # https://gist.github.com/joechrysler/6073741
     set parent_branch_name (git_parent_branch)
+    set current_branch_name (git rev-parse --abbrev-ref HEAD)
 
-    # https://github.com/spatiallabs/spatial/compare/release-8.4...merge-PLAT-1234?expand=1
-    command open $github_url"/compare/$parent_branch_name...$current_branch_name?expand=1"
+    set url (git remote -v | grep 'origin.*fetch' | awk '{print $2}')
+    set url (string replace -r 'ssh://[^@]+@' 'https://' $url)
+    set url (string replace -r 'git@github.com:' 'https://github.com/' $url | string replace '.git' '')
+
+    set url $url"/compare/$parent_branch_name...$current_branch_name?expand=1"
+
+    echo "Opening $url ..."
+
+    command open --background $url
 end
