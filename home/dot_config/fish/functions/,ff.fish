fb39adc6796c7374e2cc64db6852f05391dc4abf
Dan Sully <dsully@users.noreply.github.com>
2024-09-17 05:38:17 -0700

    fish: More fzf updates. Consolidate grep functionality.

--- home/dot_config/fish/functions/,ff.fish
+++ home/dot_config/fish/functions/,ff.fish
@@ -4,6 +4,6 @@ function ,ff --description 'Find and open files with fzf'
     fzf \
         $PICKER_ARGS \
         --bind "change:reload:$FZF_DEFAULT_COMMAND {q} || true" \
-        --bind 'enter:become(''nvim {} > "$(command tty)")' \
-        --preview "_preview_file_content {+}"
+        --bind 'enter:execute(''nvim {+} > "$(command tty)")' \
+        --preview "_preview_file_content --path {}"
 end
--- home/dot_config/fish/functions/,ft.fish
+++ home/dot_config/fish/functions/,ft.fish
@@ -1,18 +1,9 @@
-function ,ft
+function ,ft --description 'Fuzzy search for files of a specific type'
 
     if test (count $argv) -lt 2
         echo "Usage: ,ft <filetype> <query>"
         return 1
     end
 
-    set -l FZF_DEFAULT_COMMAND rg --column --line-number --no-binary --no-heading --type $argv[1]
-
-    fzf \
-        $PICKER_ARGS \
-        --bind "change:reload:$FZF_DEFAULT_COMMAND {q} || true" \
-        --bind 'enter:become(''nvim {1}:{2}:{3} > "$(command tty)")' \
-        --bind "start:reload:$FZF_DEFAULT_COMMAND '{q}'" \
-        --preview 'bat --color=always {1} --highlight-line {2}' \
-        --print-query \
-        --query $argv[2..]
+    _fzf_grep --type $argv[1] --query "$argv[2..]"
 end
