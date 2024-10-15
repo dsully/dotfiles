785950947904328377de0d7f1d2fd04bc8f20644
Dan Sully <dsully@users.noreply.github.com>
2024-10-15 04:35:31 -0700

    fish: Update config

--- home/dot_config/fish/conf.d/editor.fish
+++ home/dot_config/fish/conf.d/editor.fish
@@ -1,25 +1,21 @@
-# http://github.com/neovim/neovim
 if status is-interactive
 
     # Set VSCode to use itself if inside the VSCode terminal.
     if string match -q "$TERM_PROGRAM" vscode
         set -gx EDITOR code --wait
 
-    else if not string length --quiet "$EDITOR"
-        if command -q nvim
-            abbr --add vi nvim
-            abbr --add vim nvim
-            abbr --add view 'nvim -R'
+    else if command -q nvim
+        abbr --add vi nvim
+        abbr --add vim nvim
+        abbr --add view 'nvim -R'
 
-            set -f editor nvim
+        set -gx EDITOR nvim
 
-        else if command -q vim
-            set -f editor vim
-        else
-            set -f editor vi
-        end
-
-        set -gx EDITOR $editor
-        set -gx VISUAL $editor
+    else if command -q vim
+        set -gx EDITOR vim
+    else
+        set -gx EDITOR vi
     end
+
+    set -gx VISUAL $EDITOR
 end
