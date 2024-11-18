0d95ae2a7ff119e103b0344c52bb7517967faa00
Dan Sully <dan+github@sully.org>
2024-11-18 12:42:31 -0800

    loc: Use scc instead of tokei

--- home/dot_config/fish/functions/loc.fish
+++ home/dot_config/fish/functions/loc.fish
@@ -1,3 +1,17 @@
-function loc --description 'Run tokei with "--sort code" always' --wraps tokei
-    command tokei --sort code $argv
+function loc --description 'Run scc with "--sort code" always' --wraps scc
+    #
+    # https://github.com/boyter/scc
+    command scc \
+        --exclude-ext Makefile \
+        --exclude-ext json \
+        --exclude-ext md \
+        --exclude-ext svg \
+        --exclude-ext toml \
+        --exclude-ext yaml \
+        --no-cocomo \
+        --no-complexity \
+        --no-gen \
+        --no-size \
+        --sort code \
+        $argv
 end
