4e6a7917cbf9fc89c3b82de20c3b7b13071e34e5
Dan Sully <dsully@users.noreply.github.com>
2024-11-21 09:01:58 -0800

    fish: Update clip function

--- home/dot_config/fish/functions/clip.fish
+++ home/dot_config/fish/functions/clip.fish
@@ -1,19 +1,3 @@
 function clip --description "Copy files to clipboard"
-    set contents ""
-
-    for file in $argv
-        if test (command file $file | command grep -c -E "text|JSON") -eq 1
-            set contents "$contents\n" (command cat "$file")
-            echo "Contents of $file are added to the clipboard buffer."
-        else
-            echo "File \"$file\" is not plain text."
-        end
-    end
-
-    if test -n "$contents"
-        echo -n $contents | command pbcopy
-        echo "All valid file contents are now in the clipboard."
-    else
-        echo "No valid files to copy to the clipboard."
-    end
+    /bin/cat $argv | pbcopy
 end
