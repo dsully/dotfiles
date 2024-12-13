2a2d2d7896fa13bf97177521720e718dee9647ea
Dan Sully <dsully@users.noreply.github.com>
2024-12-13 12:56:08 -0800

    Update Brewfile

--- home/dot_config/fish/conf.d/zoxide.fish
+++ home/dot_config/fish/conf.d/zoxide.fish
@@ -9,11 +9,5 @@ if status is-interactive; and command -q zoxide
         --no-sort \
         --select-1
 
-    # Cache zoxide init output
-    set cache $XDG_CACHE_HOME/fish/conf.d/zoxide.fish
-
-    source $cache 2>/dev/null; or begin
-        zoxide init fish --cmd j >$cache
-        source $cache
-    end
+    zoxide init fish --cmd j | source
 end
