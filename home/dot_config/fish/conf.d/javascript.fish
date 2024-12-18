ca6e139a57f7a99ba61bfeeace27512184c02e3e
Dan Sully <dsully@users.noreply.github.com>
2024-12-18 12:55:12 -0800

    fish: Extract JavaScript to it's own file.

---
+++ home/dot_config/fish/conf.d/javascript.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+
+    set -gx NODE_REPL_HISTORY /dev/null
+    set -gx NO_UPDATE_NOTIFIER 1 # used by npm: https://github.com/yeoman/update-notifier/issues/180
+
+    if type -q bun
+        fish_add_path -g $XDG_CACHE_HOME/.bun/bin
+    end
+
+    if type -q volta
+        set -gx VOLTA_HOME "$HOME/.volta"
+
+        fish_add_path -g $VOLTA_HOME/bin
+    end
+
+end
