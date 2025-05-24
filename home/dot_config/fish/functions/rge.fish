bc53f3f4448dd1cca1e0e78565b61d5635b3f25e
Dan Sully <dsully@users.noreply.github.com>
2025-05-24 10:43:31 -0700

    fix(fish) rge for filenames that need escaping

--- home/dot_config/fish/functions/rge.fish
+++ home/dot_config/fish/functions/rge.fish
@@ -3,6 +3,6 @@ function rge -d "Wrapper around ripgrep to open files in $EDITOR"
     set -l matches (command rg --color=never -l $argv)
 
     if test $status -eq 0
-        eval $EDITOR $matches
+        $EDITOR $matches
     end
 end
