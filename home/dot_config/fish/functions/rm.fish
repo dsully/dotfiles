b2bd6d6165324e02127712c4fc15c73f59c3d623
Dan Sully <dsully@users.noreply.github.com>
2025-06-21 07:23:02 -0700

    fix(fish): simplify rm wrapper

--- home/dot_config/fish/functions/rm.fish
+++ home/dot_config/fish/functions/rm.fish
@@ -1,57 +1,32 @@
-function clean_rm_flags
-    set -l cleaned
+function rm --wraps rip
+    set -l cmd /bin/rm
+    set -l options
+
+    if type -q rip
+        set cmd rip
+    else if type -q gomi
+        set cmd gomi
+    end
+
+    if string match -q -- /bin/rm $cmd
+        /bin/rm $argv
+        return
+    end
 
     for arg in $argv
+
         if string match -q -- --version $arg
-            echo $arg
+            $cmd $arg
             return
         end
 
         # Skip recursive and force flags in various forms
         if string match -q -- -r $arg; or string match -q -- -f $arg; or string match -q -- -rf $arg; or string match -q -- -fr $arg
             continue
-
-            # Handle other flags that might contain r or f
-        else if string match -q -- "-*" $arg
-            # Check if the flag contains 'r' or 'f'
-            set cleaned_flag (string replace -r -a '[rf]' '' -- $arg)
-
-            # set cleaned_flag (string replace -a "r" "" (string replace -a "f" "" $arg))
-
-            # If the cleaned flag is different from the original and not just a hyphen
-            if test "$cleaned_flag" != "$arg" -a "$cleaned_flag" != -
-                set -a cleaned $cleaned_flag
-
-            else if test "$cleaned_flag" = -
-                # Skip if only r and f were in the flag
-                continue
-            else
-                # Keep the flag as is
-                set -a cleaned $arg
-            end
-        else
-            # Add non-flag arguments as is
-            set -a cleaned $arg
         end
+
+        set -a options $arg
     end
 
-    if test -z "$cleaned"
-        echo "Error: cleaned argv is empty! Original: [$argv]" >&2
-        exit 1
-    end
-
-    echo $cleaned
-end
-
-function rm --wraps rip
-    if type -q rip
-
-        command rip (clean_rm_flags $argv)
-
-    else if type -q gomi
-
-        command gomi $argv
-    else
-        /bin/rm $argv
-    end
+    $cmd $options
 end
