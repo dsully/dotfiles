697b0b5fbb6e875c3fe850b5cf54ef72b158a1ee
Dan Sully <dsully@users.noreply.github.com>
2025-06-27 07:16:05 -0700

    fix: add biome config path

--- home/dot_config/fish/conf.d/javascript.fish
+++ home/dot_config/fish/conf.d/javascript.fish
@@ -3,6 +3,10 @@ if status is-interactive
     set -gx NODE_REPL_HISTORY /dev/null
     set -gx NO_UPDATE_NOTIFIER 1 # used by npm: https://github.com/yeoman/update-notifier/issues/180
 
+    if test -f $XDG_CONFIG_HOME/biome.json
+        set -gx BIOME_CONFIG_PATH $XDG_CONFIG_HOME/biome.json
+    end
+
     if type -q volta
         set -gx VOLTA_HOME "$HOME/.volta"
 
