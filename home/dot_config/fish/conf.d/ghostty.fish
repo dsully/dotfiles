88056f715ca368b33931208433aab09fec72f4c9
Dan Sully <dsully@users.noreply.github.com>
2025-07-06 07:14:01 -0700

    misc: updates

--- home/dot_config/fish/conf.d/ghostty.fish
+++ home/dot_config/fish/conf.d/ghostty.fish
@@ -1,7 +1,5 @@
 if status is-interactive; and test -n "$GHOSTTY_RESOURCES_DIR"
 
-    set -p fish_complete_path (path resolve "$GHOSTTY_RESOURCES_DIR/../fish/vendor_completions.d")
-
     # Set up Ghostty's shell integration.
     source $GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish
 end
