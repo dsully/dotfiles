8fb06c1c6086991a0483d3b47a8803baec395502
Dan Sully <dsully@users.noreply.github.com>
2025-11-22 12:27:37 -0800

    fix: GH tokens

--- home/dot_config/fish/conf.d/zz-last.fish
+++ home/dot_config/fish/conf.d/zz-last.fish
@@ -1,1 +1,3 @@
-set -gx NIX_CONFIG "access-tokens = github.com=$GH_TOKEN"
+set -gx NIX_CONFIG "access-tokens = github.com=$GITHUB_TOKEN"
+set -gx GH_TOKEN $GITHUB_TOKEN
+set -gx GITHUB_API_TOKEN $GITHUB_TOKEN
